de2fd3d899f14571c371c6c22b6209b7b7e357dc
Kimserey <kimserey.lam@groomgy.com>
2018-03-18 00:23:46 +0800

    Added Vault Sharp for token authentication

--- VaultTest/Startup.cs
+++ VaultTest/Startup.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using VaultSharp;
+using VaultSharp.Backends.Authentication.Models;
+using VaultSharp.Backends.Authentication.Models.Token;
 
 namespace VaultTest
 {
@@ -29,9 +32,16 @@ namespace VaultTest
                 app.UseDeveloperExceptionPage();
             }
 
+            IVaultClient vaultClient = VaultClientFactory.CreateVaultClient(
+                new Uri("http://localhost:8200"), 
+                new TokenAuthenticationInfo("73045cbb-af51-2dd9-e2aa-031292988586"));
+
+
+            var secret = vaultClient.ReadSecretAsync("secret/test").Result;
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync(_configuration["Test"]);
+                await context.Response.WriteAsync(secret.Data["test"].ToString());
             });
         }
     }
