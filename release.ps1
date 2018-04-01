3716de85445f14c3b2a468be43de55178907014f
Kimserey <kimserey.lam@groomgy.com>
2018-04-01 09:20:43 +0800

    bump version

---
+++ release.ps1
@@ -0,0 +1,8 @@
+$version = gitversion /showvariable semver
+[xml]$props = Get-Content .\Directory.build.props
+$props.Project.PropertyGroup.Version = "$version"
+$props.Save(".\Directory.build.props")
+git add .
+git commit -m "bump version"
+git tag $version
+git push origin $version
