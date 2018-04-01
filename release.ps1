7dd4bfc39a5ea7566d323b03e2811df55f0d5ef2
Kimserey <kimserey.lam@groomgy.com>
2018-04-01 13:36:58 +0800

    Release script

--- release.ps1
+++ release.ps1
@@ -1,8 +1,22 @@
+# Get the version from master branch
+# by running gitversion.
+# Make sure Gitversion is registered in PATH.
+
+git checkout master
 $version = gitversion /showvariable semver
-[xml]$props = Get-Content .\Directory.build.props
+
+# Update Directory Build Properties version in root.
+# Version Property dictate all versions of all *.dlls.
+
+$propsPath = ".\Directory.build.props"
+[xml]$props = Get-Content $propsPath
 $props.Project.PropertyGroup.Version = "$version"
-$props.Save(".\Directory.build.props")
-git add .
-git commit -m "bump version"
+$props.Save($propsPath)
+
+# Commit Bump version
+# And tag commit
+
+git add $propsPath
+git commit -m "Bump version for release"
 git tag $version
 git push origin $version --porcelain
